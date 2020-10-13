6729829b6229a08d5f8525466b48bc0ba3ca38fa
Cai Nowicki <cai.nowicki@gmail.com>
2020-10-12 23:46:57 -0400

    updated getitem to link to correct row/column on edges

--- game_objects.py
+++ game_objects.py
@@ -13,12 +13,23 @@ class Grid:
             for j in range(n):
                 self.grid[i][j] = create_cell()
         self.generation = 0
+        self.n = n
+
 
     def __getitem__(self, i, j):
+        if i == self.n:
+            i = 0
+        if j == self.n:
+            j = 0
+        if i < 0:
+            i = self.n - 1
+        if j < 0:
+            j = self.n - 1
         return self.grid[i][j]
 
     def iterate_generation(self):
         self.generation += 1
+
 
 class Cell:
     def __init__(self):
