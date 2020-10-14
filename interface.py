0adc28b3b3f5bef70860a56c324c52a718ddb001
Cai Nowicki <cai.nowicki@gmail.com>
2020-10-13 22:26:00 -0400

    more work on Plotly interface

--- interface.py
+++ interface.py
@@ -1,33 +1,54 @@
 import plotly.graph_objects as go
-from game_objects import Cell
 
-cell= Cell()
+from game_objects import Cell, Grid
 
-def create_grid(n):
-    x = [i + 0.5 for i in range(n)]
-    y = [i + 0.5 for i in range(n)]
-    fig = go.Figure(go.Scatter(x=x, y=y, marker = dict(size=25, symbol=1), line=dict(width=0)))
-    #if n = 25, this fills whole square
-    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor = "black", tickvals = [x for x in range(n)],
-                     range=[0,n],  scaleanchor="x", constrain="domain")
-    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor = "black", tickvals = [x for x in range(n)],
-                     range=[0,n], constrain="domain", scaleanchor="y")
-    fig.update_layout(hovermode=False, clickmode="event")
-#TODO
-#adjust size of marker dynamically so it fills block regardless of n
-#figure out how to capture mouseclicks on graph in Python
 
-def update_cell(cell):
-    #if detects a mouse click
-    #determine which block contains mouse click
-    #check if clickable == True
-    #then call cell.change_state()
-    pass
+def create_grid(n, grid):
+    x = [i for i in range(n)]
+    y = [i for i in range(n)]
+    traces = []
+    for value_x in x:
+        for value_y in y:
+            cell = grid.__getitem__(value_x, value_y)
+            trace = go.Scatter(x=[value_x + 0.5], y=[value_y + 0.5],
+                               marker=dict(size=30, symbol=1, color="blue"), line=dict(width=0))
+            traces.append(trace)
+    fig = go.FigureWidget(data=traces)
+    fig.update_layout(hovermode="closest", clickmode="event", plot_bgcolor="white", width=1000, height=1000, showlegend=False)
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor="black", tickvals=[x for x in range(n)],
+                     range=[0, n], scaleanchor="x", constrain="domain")
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor="black", tickvals=[x for x in range(n)],
+                     range=[0, n], constrain="domain", scaleanchor="y")
+    scatter = fig.data[0]
+
+    def update_cell(trace, points, selector):
+        x = trace.x - 0.5
+        y = trace.y - 0.5
+        c = list(scatter.marker.color)
+        cell = grid.__getitem__(x, y)
+        cell.change_state()
+        for i in points.points_inds:
+            c[i] = "black"
+            with fig.batch_update():
+                scatter.marker.color = c
+
+
+    scatter.on_click(update_cell)
+    fig.show()
+
+
+# TODO
+# adjust size of marker dynamically so it fills block regardless of n
+# figure out how to capture mouseclicks on graph in Python
+
 
 def clickable(grid):
-    #if simulation has started
-    #change cell.clickable to False for all cells in grid
-    #if simulation has ended
-    #change cell.clickable to True for all cells in grid
+    # if simulation has started
+    # change cell.clickable to False for all cells in grid by calling cell.change_click()
+    # if simulation has ended
+    # change cell.clickable to True for all cells in grid by calling cell.change_click()
     pass
-create_grid(5)
+
+
+grid = Grid(25)
+create_grid(25, grid)
