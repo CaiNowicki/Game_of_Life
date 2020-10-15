50c7d0d2811f046ab33d6ba42f867062f09c7c4f
Cai Nowicki <cai.nowicki@gmail.com>
2020-10-14 23:29:42 -0400

    created clickable graph interface, need dash framework to make it interactive

---
+++ app/app.py
@@ -0,0 +1,13 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from app_scripts.interface import create_grid
+from app_scripts.game_objects import Cell, Grid
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
