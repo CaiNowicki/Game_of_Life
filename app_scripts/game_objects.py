185a056f14d51f6493cdc8497da824cff626c3bd
Cai Nowicki <53185634+dunkelweizen@users.noreply.github.com>
2020-10-22 01:31:02 -0400

    Pygame method (#5)
    * still working on graph display in Dash app
    
    * trying to get Dash app to update as grid updates
    
    * uncommenting callback code, app has problems
    
    * corrected interface function
    
    * making sure up to date
    
    * app loads single randomized starting grid
    
    * created interatcive matplotlib graph, trying for dash integreation
    
    * trying pygame instead of plotly
    
    * it's interactive and it runs!

--- app_scripts/game_objects.py
+++ app_scripts/game_objects.py
@@ -1,9 +1,13 @@
 import numpy as np
+import random
 
 
 def create_cell():
     cell = Cell()
     return cell
+
+
+
 
 
 class Grid:
@@ -26,15 +30,57 @@ class Grid:
             j = self.n - 1
         return self.grid[i][j]
 
-    def iterate_generation(self):
+    def update_grid(self):
         self.generation += 1
+        for i in range(self.n):
+            for j in range(self.n):
+                cell = self.__getitem__(i, j)
+                neighbors = self.find_neighbors(i, j)
+                live_neighbors = 0
+                # count how many live neighbors
+                # if alive and 2 or 3 live neighbors, stay alive
+                # if alive and <2 live neighbors, die
+                # if alive and >3 live neighbors, die
+                # if dead and 3 live neighbors, become alive
+                # else state remains unchanged
+                for neighbor in neighbors:
+                    if neighbor.state:
+                        live_neighbors += 1
+                #if cell is currently alive, it dies with fewer than 2 neighbors or more than 3
+                if cell.state:
+                    if 0 < live_neighbors <2:
+                        cell.change_state()
+                        print(f'Cell {i},{j} has died')
+                        print(cell.state)
+                    elif 4 <= live_neighbors:
+                        cell.change_state()
+                        print(f'Cell {i}, {j} has died')
+                        print(cell.state)
+                #if cell is currently dead, it comes alive with exactly 3 neighbors
+                else:
+                    if live_neighbors == 3:
+                        cell.change_state()
 
+
+    def find_neighbors(self, i, j):
+        neighbors = []
+        neighbors.append(self.__getitem__(i - 1, j - 1))
+        neighbors.append(self.__getitem__(i - 1, j))
+        neighbors.append(self.__getitem__(i, j - 1))
+        neighbors.append(self.__getitem__(i + 1, j - 1))
+        neighbors.append(self.__getitem__(i - 1, j + 1))
+        neighbors.append(self.__getitem__(i, j + 1))
+        neighbors.append(self.__getitem__(i + 1, j))
+        neighbors.append(self.__getitem__(i + 1, j + 1))
+        return neighbors
 
 class Cell:
     def __init__(self):
         self.state = False
-        self.color = "white"
-        self.clickable = True
+        if self.state:
+            self.color = "black"
+        else:
+            self.color = "white"
 
     def change_state(self):
         if self.state:
